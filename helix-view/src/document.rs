6a2c38766f19c0c052e2e36bc0e66510a3736b35
vgwidt <vgwidt@live.com>
2023-09-03 16:21:27 +0900

    Fix swap being made in file picker preview

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -701,7 +701,7 @@ impl Document {
         // Check if files exist
         let swap_file_exists = swap_file_path.exists();
         let original_file_exists = path.exists();
-    
+
         // Decide what to do based on existence of files
         let (rope, encoding, has_bom) = if swap_file_exists {
             if original_file_exists {
@@ -709,43 +709,76 @@ impl Document {
                 bail!("Conflict detected: Swap file '{}' and original file '{}' both exist. Resolve the conflict before editing.", swap_file_path.display(), path.display());
             } else {
                 // Swap file exists, but no original file (potentially crashed without saving, continue editing)
-                let mut file = std::fs::File::open(&swap_file_path).context(format!("unable to open {:?}", swap_file_path))?;
+                let mut file = std::fs::File::open(&swap_file_path)
+                    .context(format!("unable to open {:?}", swap_file_path))?;
                 from_reader(&mut file, encoding)?
             }
         } else if original_file_exists {
             // Path exists; create a new swap file from the original file
-            std::fs::copy(path, &swap_file_path).context(format!("unable to create swap file {:?} from {:?}", swap_file_path, path))?;
-    
+            std::fs::copy(path, &swap_file_path).context(format!(
+                "unable to create swap file {:?} from {:?}",
+                swap_file_path, path
+            ))?;
+
             // Open the newly created swap file for editing
-            let mut file = std::fs::File::open(&swap_file_path).context(format!("unable to open {:?}", swap_file_path))?;
+            let mut file = std::fs::File::open(&swap_file_path)
+                .context(format!("unable to open {:?}", swap_file_path))?;
             from_reader(&mut file, encoding)?
         } else {
             // Neither swap nor original file exists; create a new empty swap file
-            std::fs::File::create(&swap_file_path).context(format!("unable to create swap file {:?}", swap_file_path))?;
+            std::fs::File::create(&swap_file_path)
+                .context(format!("unable to create swap file {:?}", swap_file_path))?;
             let line_ending: LineEnding = config.load().default_line_ending.into();
             let encoding = encoding.unwrap_or(encoding::UTF_8);
             (Rope::from(line_ending.as_str()), encoding, false)
         };
-    
+
         let mut doc = Self::from(rope, Some((encoding, has_bom)), config);
-    
+
         // Set paths
         // TO-DO: Create fetter for swap_path
         doc.set_path(Some(&path.to_path_buf()));
         doc.swap_path = Some(swap_file_path);
-    
+
         // Try detecting the language
         if let Some(loader) = config_loader {
             doc.detect_language(loader);
         }
-    
+
         doc.detect_indent_and_line_ending();
-    
+
         Ok(doc)
     }
-    
-    
 
+    pub fn open_preview(
+        path: &Path,
+        encoding: Option<&'static Encoding>,
+        config_loader: Option<Arc<syntax::Loader>>,
+        config: Arc<dyn DynAccess<Config>>,
+    ) -> Result<Self, Error> {
+        // Open the file if it exists, otherwise assume it is a new file (and thus empty).
+        let (rope, encoding, has_bom) = if path.exists() {
+            let mut file =
+                std::fs::File::open(path).context(format!("unable to open {:?}", path))?;
+            from_reader(&mut file, encoding)?
+        } else {
+            let line_ending: LineEnding = config.load().default_line_ending.into();
+            let encoding = encoding.unwrap_or(encoding::UTF_8);
+            (Rope::from(line_ending.as_str()), encoding, false)
+        };
+
+        let mut doc = Self::from(rope, Some((encoding, has_bom)), config);
+
+        // set the path and try detecting the language
+        doc.set_path(Some(path));
+        if let Some(loader) = config_loader {
+            doc.detect_language(loader);
+        }
+
+        doc.detect_indent_and_line_ending();
+
+        Ok(doc)
+    }
     /// The same as [`format`], but only returns formatting changes if auto-formatting
     /// is configured.
     pub fn auto_format(&self) -> Option<BoxFuture<'static, Result<Transaction, FormatterError>>> {
@@ -898,7 +931,7 @@ impl Document {
                 let path = name_swap_file(&path);
                 self.swap_path = Some(path.clone());
                 path
-            },
+            }
         };
 
         let identifier = self.path().map(|_| self.identifier());
@@ -936,11 +969,10 @@ impl Document {
                 }
             }
 
-
             // Create or open the swap file and write to it
             let mut swap_file = File::create(swap_path.as_path()).await?;
             to_writer(&mut swap_file, encoding_with_bom_info, &text).await?;
-            
+
             // Copy swap file to path
             std::fs::copy(swap_path.as_path(), &path)?;
 
@@ -1023,7 +1055,7 @@ impl Document {
             // Create or open the swap file and write to it
             let mut swap_file = File::create(&swap_path.as_path()).await?;
             to_writer(&mut swap_file, encoding_with_bom_info, &text).await?;
-            
+
             let event = DocumentSavedEvent {
                 revision: current_rev,
                 doc_id,
@@ -1049,7 +1081,6 @@ impl Document {
 
         Ok(future)
     }
-
 
     /// Detect the programming language based on the file type.
     pub fn detect_language(&mut self, config_loader: Arc<syntax::Loader>) {
@@ -1952,7 +1983,7 @@ impl Document {
     }
 }
 
-fn name_swap_file(path: &Path) -> PathBuf {
+pub fn name_swap_file(path: &Path) -> PathBuf {
     let file_name = path.file_name().expect("Invalid path");
     let swap_file_name = format!(".{}.hxs", file_name.to_str().expect("Invalid file name"));
     let swap_file_path = path.with_file_name(swap_file_name);
